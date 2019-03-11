36a8b2ca9f231b9654b99b643c2e4b881dbd58f7
Angelo Maia <angelomaia@id.uff.br>
2019-03-11 18:10:14 -0300

    Estetica

--- calc.py
+++ calc.py
@@ -3,12 +3,12 @@ from tkinter import *
 import subprocess
 
 #a tela foi dividida em dois frames, onde os botoes devem ser distribuidos
-root = Tk()						
-frame = Frame(root)  
+root = Tk()				
+root.iconbitmap('bolt.ico')		
+frame = Frame(root,width=100, height=100)  
 frame.pack()
-bottomframe = Frame(root)
+bottomframe = Frame(root,width=100, height=200)
 bottomframe.pack( side = BOTTOM )
-bottomframe.pack(side = BOTTOM)
 
 #====Frame Layout====#
 frame.option_add('*Font', 'tahoma 20 bold')
@@ -18,8 +18,8 @@ frame.pack(expand = NO, fill = BOTH)
 
 #display
 #====Exibir Display====#
-display = Entry(frame, bd = 5, justify='right', font="tahoma")
-display.pack(padx=10, pady=20, side=LEFT)
+display = Entry(frame, bd = 5, justify='center', font="tahoma")
+display.pack(padx=30, pady=20, side=LEFT)
 
 #botoes
 #====Limpar o Display====#
@@ -40,7 +40,7 @@ dreiButton.pack(side=LEFT)
 
 
 #====Executar um .bat====#
-open_expButton = Button(bottomframe, text="exp", font="tahoma", command = lambda: subprocess.call(['open_explorer.bat']))
+open_expButton = Button(bottomframe, text="rodar .bat", font="tahoma", command = lambda: subprocess.call(['open_explorer.bat']))
 open_expButton.pack(side=LEFT)
 
  
