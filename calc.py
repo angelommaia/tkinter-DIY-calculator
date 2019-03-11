5c140c897f6a06cda00d384487a82d8f06384e9c
Angelo Maia <angelomaia@id.uff.br>
2019-03-11 16:20:03 -0300

    Some Buttons

--- calc.py
+++ calc.py
@@ -6,17 +6,22 @@ root = Tk()
 frame = Frame(root)  
 frame.pack()
 bottomframe = Frame(root)
+bottomframe.pack( side = BOTTOM )
 bottomframe.pack(side = BOTTOM)
 
 #====Frame Layout====#
 frame.option_add('*Font', 'tahoma 20 bold')
+frame.master.title('Simple Calculator')
 frame.master.title('Simple Calculator')		#título da janela
 frame.pack(expand = NO, fill = BOTH)
 
+#display
 #====Exibir Display====#
 display = Entry(frame, bd = 5, justify='right', font="tahoma")
 display.pack(side = LEFT)
 
+#botoes
+clearButton = Button(frame, text="C", font="tahoma", fg="blue", command = lambda: display.delete(0,"end"))
 #====Limpar o Display====#
 #lambda permite o uso de variaveis locais em outras partes do programa
 clearButton = Button(frame, text="C", font="tahoma", fg="blue", command = lambda: display.delete(0,"end")) 
@@ -26,10 +31,19 @@ clearButton.pack(side=LEFT)
 equalButton = Button(bottomframe, text="=", font="tahoma", command = lambda: avaliar_eq(display))
 equalButton.pack(side=LEFT)
 
+einButton = Button(bottomframe, text="1", font="tahoma", command = lambda: display.insert("end","1"))
+einButton.pack(side=LEFT)
+zweiButton = Button(bottomframe, text="2", font="tahoma", command = lambda: display.insert("end","2"))
+zweiButton.pack(side=LEFT)
+dreiButton = Button(bottomframe, text="3", font="tahoma", command = lambda: display.insert("end","3"))
+dreiButton.pack(side=LEFT)
 
 def avaliar_eq(display):
+	text=eval(display.get())
+	display.delete(0,"end")
+	display.insert(0,text)
 	text=eval(display.get()) #interpreta o que esta escrito no display (eval()) e salva o resultado em uma variavel 
 	display.delete(0,"end")  #limpa o display
 	display.insert(0,text)   #insere no display o resultado obtido anteriormente
-	
-root.mainloop()
+
+root.mainloop() 
